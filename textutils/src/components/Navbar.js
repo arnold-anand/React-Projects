eecfcb77a3835e67e62e78efbfe90640c08f43aa
arnold-anand <pagadalaarnold@karunya.edu.in>
2023-05-30 01:20:15 +0530

    text utils completed

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -2,17 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
-    <nav className='flex justify-between items-center p-2'>
+    <nav className='flex justify-between items-center p-2 bg-[#222] text-white'>
         <div className='text-2xl'>{props.title}</div>
-        <div>
-            <ul className='flex space-x-5'>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">About</a></li>
-            </ul>
-        </div>
         <div className='space-x-3'>
-            <input className='border border-green-300 p-2 rounded-md' type="search" name="" id="" />
-            <button className='border border-green-500 text-green-700 p-2 hover:bg-green-200 rounded-md'>Search</button>
+            <input className='border border-blue-300 p-2 rounded-md' type="search" name="" id="" />
+            <button className='border border-blue-500 text-blue-700 p-2 hover:bg-slate-300 rounded-md'>Search</button>
         </div>
     </nav>
   )
--- textutils/src/components/TextArea.js
+++ textutils/src/components/TextArea.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, {useState} from 'react'
 export default function TextArea(props) {
+    const [text, setText] = useState("Enter text here")
+    const handleUppercase = () =>{
+        let newText = text.toUpperCase()
+        setText(newText)
+       
+    }
+    const handleLowercase = () =>{
+        let newText = text.toLowerCase()
+        setText(newText)
+       
+    }
+    const handleOnChange = (event) =>{
+        setText(event.target.value)
+    }
   return (
-    <div className='flex justify-center items-center flex-col'>
+    <div className='flex justify-center items-center'>
+    <div className='flex justify-center flex-col w-96'>
         <div className='text-3xl'>{props.heading}</div>
-        <textarea className='border border-blue-300 m-2' name="" id="" cols="80" rows="10"></textarea>
-        <button className='border p-2 border-blue-300 rounded-lg'>Convert to Uppercase</button>
+        <textarea onChange={handleOnChange} className='border border-blue-300 m-2' name="" id="" cols="80" rows="10" value={text}></textarea>
+        <button onClick={handleUppercase} className='border p-2 border-blue-300 rounded-lg m-2'>Convert to Uppercase</button>
+        <button onClick={handleLowercase} className='m-2 border p-2 border-blue-300 rounded-lg'>Convert to Lowercase</button>
+        <div className='my-5 space-y-1'>
+            <div className="text-3xl">Your Text Summary</div>
+            <br />
+            <div>{text.split(" ").length} words and {text.length} characters</div>
+            <div>{text.split(" ").length * 0.008} Minutes taken to read</div>
+            <div className='text-3xl'>Preview</div>
+            <div>{text}</div>
+        </div>
+    </div>
     </div>
   )
 }
