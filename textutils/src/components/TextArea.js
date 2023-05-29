5ebeb54b6f1a7cfa6746a2f8c332337c249ab4b3
arnold-anand <pagadalaarnold@karunya.edu.in>
2023-05-30 01:22:47 +0530

    Clear Text Feature added

--- textutils/src/components/TextArea.js
+++ textutils/src/components/TextArea.js
@@ -11,6 +11,11 @@ export default function TextArea(props) {
         setText(newText)
        
     }
+    const handleClearText = () =>{
+
+        setText("")
+       
+    }
     const handleOnChange = (event) =>{
         setText(event.target.value)
     }
@@ -21,6 +26,7 @@ export default function TextArea(props) {
         <textarea onChange={handleOnChange} className='border border-blue-300 m-2' name="" id="" cols="80" rows="10" value={text}></textarea>
         <button onClick={handleUppercase} className='border p-2 border-blue-300 rounded-lg m-2'>Convert to Uppercase</button>
         <button onClick={handleLowercase} className='m-2 border p-2 border-blue-300 rounded-lg'>Convert to Lowercase</button>
+        <button onClick={handleClearText} className='m-2 border p-2 border-blue-300 rounded-lg'>Clear Text</button>
         <div className='my-5 space-y-1'>
             <div className="text-3xl">Your Text Summary</div>
             <br />
