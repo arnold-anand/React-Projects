b0b0802e16d13b9745357c6d4250529b86b320cf
arnold-anand <pagadalaarnold@karunya.edu.in>
2023-05-31 19:09:42 +0530

    Added React Router

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextArea from './components/TextArea';
+
+import {
+  BrowserRouter as Router, Routes, Route
+} from "react-router-dom";
 function App() {
 
   return (
     <>
     <Navbar title="TextUtils"></Navbar>
-    <TextArea heading="Enter your text here"></TextArea>
+    <Router>
+    <Routes>
+          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/" element={<TextArea heading="Enter your text here"></TextArea>}/>
+    </Routes>
+    </Router>
     </>
   );
 }
